49c5c8881f44fc6dde4cf4ccc22128599f49e09e
Nate <150736820+NateHerrera@users.noreply.github.com>
2025-10-21 20:40:48 -0500

    Basic two section layout, button implementation next

--- cookbook/lib/main.dart
+++ cookbook/lib/main.dart
@@ -10,10 +10,38 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: CookbookLayout(),
+    );
+  }
+}
+
+class CookbookLayout extends StatelessWidget {
+  const CookbookLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // left side
+          Container(
+            width: 220,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              border: Border.all(color: Colors.black, width: 1),
+            ),
+          ),
+          Expanded(
+            // right side
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black, width: 1),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
