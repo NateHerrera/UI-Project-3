9798ae880e85141c139399e6934d87679c68500e
Nate <150736820+NateHerrera@users.noreply.github.com>
2025-10-24 17:29:59 -0500

    buttons for testing

--- cookbook/lib/main.dart
+++ cookbook/lib/main.dart
@@ -31,6 +31,47 @@ class CookbookLayout extends StatelessWidget {
               color: Colors.grey[300],
               border: Border.all(color: Colors.black, width: 1),
             ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: cookBookButton,
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(),
+                    padding: const EdgeInsets.all(25),
+                  ),
+                  icon: const Icon(Icons.menu_book, color: Colors.black),
+                  label: const Text(
+                    'Cook Book',
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                OutlinedButton.icon(
+                  onPressed: addRecipe,
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(),
+                    padding: const EdgeInsets.all(25),
+                  ),
+                  icon: const Icon(Icons.add_box, color: Colors.black),
+                  label: const Text(
+                    'Add Recipes',
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                OutlinedButton.icon(
+                  onPressed: shoppingList,
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(),
+                    padding: const EdgeInsets.all(25),
+                  ),
+                  icon: const Icon(Icons.shopping_cart, color: Colors.black),
+                  label: const Text(
+                    'Shopping List',
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+              ]
+            )
           ),
           Expanded(
             // right side
@@ -45,4 +86,16 @@ class CookbookLayout extends StatelessWidget {
       ),
     );
   }
+}
+
+void cookBookButton() {
+  print("Cook Book");
+}
+
+void addRecipe() {
+  print("Add Recipe");
+}
+
+void shoppingList() {
+  print("Shopping List");
 }
